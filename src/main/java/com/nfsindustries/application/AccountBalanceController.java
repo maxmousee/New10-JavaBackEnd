c89e9af3374f629a70a1a2123c94ae007a0ed582
Natan Facchin <maxmousee@gmail.com>
2018-11-13 01:09:24 +0100

    Removed unused class and added XSD validation method

--- src/main/java/com/nfsindustries/application/AccountBalanceController.java
+++ src/main/java/com/nfsindustries/application/AccountBalanceController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.InputStream;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
@@ -36,5 +42,22 @@ public class AccountBalanceController {
     @ExceptionHandler(StorageFileNotFoundException.class)
     public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
         return ResponseEntity.notFound().build();
+    }
+
+    private static boolean validateAgainstXSD(InputStream xml, InputStream xsd)
+    {
+        try
+        {
+            SchemaFactory factory =
+                    SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new StreamSource(xsd));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(xml));
+            return true;
+        }
+        catch(Exception ex)
+        {
+            return false;
+        }
     }
 }
